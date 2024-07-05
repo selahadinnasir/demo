36fcc00989b17206eb4334cd1f0785dae5a428aa
selahadinnasir <selahadinas93@gmail.com>
2024-07-05 18:07:56 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", function () {
+    var toggler = document.getElementsByClassName("caret");
+    for (var i = 0; i < toggler.length; i++) {
+        toggler[i].addEventListener("click", function () {
+            this.parentElement.querySelector(".nested").classList.toggle("active");
+            this.classList.toggle("caret-down");
+        });
+    }
+});
